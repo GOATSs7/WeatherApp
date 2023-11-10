f5f70a14750f9f5723a41b52a5f687a381dc7c23
sangram chopdar <sangramdadasochopdar@gmail.com>
2023-11-10 23:02:03 +0530

    update in timeformat

--- script.js
+++ script.js
@@ -23,18 +23,41 @@ async function getWeatherInfo(city) {
 async function getWeather(city) {
   const cityName = await getWeatherInfo(city);
 
+  document.getElementById("weather-info").innerHTML = "";
   // data to html
+  document.getElementById("temp").innerHTML = cityName.temp;
 
   document.getElementById("cloud_pct").innerHTML = cityName.cloud_pct;
+
   document.getElementById("feels_like").innerHTML = cityName.feels_like;
+
   document.getElementById("humidity").innerHTML = cityName.humidity;
+
   document.getElementById("max_temp").innerHTML = cityName.max_temp;
+
   document.getElementById("min_temp").innerHTML = cityName.min_temp;
-  document.getElementById("sunrise").innerHTML = cityName.sunrise;
-  document.getElementById("sunset").innerHTML = cityName.sunset;
-  document.getElementById("temp").innerHTML = cityName.temp;
+
   document.getElementById("wind_degrees").innerHTML = cityName.wind_degrees;
+
   document.getElementById("wind_speed").innerHTML = cityName.wind_speed;
+
+  // Convert the timestamp 1699663070 seconds to hours in AM/PM format
+
+  function convertSecondsToTime(seconds) {
+    const date = new Date(seconds * 1000); // Convert seconds to milliseconds
+    const options = { hour: "numeric", minute: "numeric", hour12: true };
+    return date.toLocaleTimeString("en-US", options);
+  }
+
+  // Example usage
+  const sunriseInSeconds = cityName.sunrise; // Replace with your actual sunrise time in seconds
+  const sunsetInSeconds = cityName.sunset; // Replace with your actual sunset time in seconds
+
+  const sunriseFormatted = convertSecondsToTime(sunriseInSeconds);
+  const sunsetFormatted = convertSecondsToTime(sunsetInSeconds);
+
+  document.getElementById("sunrise").innerHTML = sunriseFormatted;
+  document.getElementById("sunset").innerHTML = sunsetFormatted;
 
   // Add if else statement to show bad weather if cloud cover percentage is above 50
   if (cityName.cloud_pct <= 10) {
